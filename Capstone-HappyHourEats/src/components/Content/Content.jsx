d7aa529ecfed95772547ac1e4c2845fa78a75ef6
Jenny Nguyen <uyen.nnd@gmail.com>
2024-01-06 14:36:50 -0800

    Update frontend to show restaurants

--- Capstone-HappyHourEats/src/components/Content/Content.jsx
+++ Capstone-HappyHourEats/src/components/Content/Content.jsx
@@ -1,84 +1,55 @@
 import React from 'react';
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./Content.scss";
 import resImg from "../../assets/mainImg.jpg";
 
 function Content() {
-    return (
-        <>
+	const [restaurants, setRestaurants] = useState([]);
 
-            <div className="shows__section-container">
-                <li className="shows__container">
-                    <div className="shows__container-buffer">
-                        <div className="shows__headings-container">
-                            <p className="shows__heading"></p>
-                            <img className="shows__description--img" src={resImg} alt="res Img"/>
-                        </div>
-                        <div className="shows__headings-container">
-                                <p className="shows__heading">Restaurant Name</p>
-                                <p className="shows__description">Montana BBQ</p>
-                        </div>
-                        <div className="shows__headings-container">
-                                <p className="shows__heading">Location</p>
-                                <p className="shows__description-location">Vancouver</p>
-                        </div>
-                                <button className="shows__button">See Happy Hours Menu</button>
-                        </div>
-                </li>
-                <li className="shows__container">
-                    <div className="shows__container-buffer">
-                        <div className="shows__headings-container">
-                            <p className="shows__heading"></p>
-                            <img className="shows__description--img" src={resImg} alt="res Img"/>
-                        </div>
-                        <div className="shows__headings-container">
-                                <p className="shows__heading">Restaurant Name</p>
-                                <p className="shows__description">Montana BBQ</p>
-                        </div>
-                        <div className="shows__headings-container">
-                                <p className="shows__heading">Location</p>
-                                <p className="shows__description-location">Vancouver</p>
-                        </div>
-                                <button className="shows__button">See Happy Hours Menu</button>
-                        </div>
-                </li>
-                <li className="shows__container">
-                    <div className="shows__container-buffer">
-                        <div className="shows__headings-container">
-                            <p className="shows__heading"></p>
-                            <img className="shows__description--img" src={resImg} alt="res Img"/>
-                        </div>
-                        <div className="shows__headings-container">
-                                <p className="shows__heading">Restaurant Name</p>
-                                <p className="shows__description">Montana BBQ</p>
-                        </div>
-                        <div className="shows__headings-container">
-                                <p className="shows__heading">Location</p>
-                                <p className="shows__description-location">Vancouver</p>
-                        </div>
-                                <button className="shows__button">See Happy Hours Menu</button>
-                        </div>
-                </li>
-                <li className="shows__container">
-                    <div className="shows__container-buffer">
-                        <div className="shows__headings-container">
-                            <p className="shows__heading"></p>
-                            <img className="shows__description--img" src={resImg} alt="res Img"/>
-                        </div>
-                        <div className="shows__headings-container">
-                                <p className="shows__heading">Restaurant Name</p>
-                                <p className="shows__description">Montana BBQ</p>
-                        </div>
-                        <div className="shows__headings-container">
-                                <p className="shows__heading">Location</p>
-                                <p className="shows__description-location">Vancouver</p>
-                        </div>
-                                <button className="shows__button">See Happy Hours Menu</button>
-                        </div>
-                </li>
-            </div>
-                                   
-        </>
-    )
+	const fetchRestaurants = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8080/restaurants`);
+      const restaurants = response.data;
+      setRestaurants(restaurants);
+    } catch (error) {
+      console.log("Error fetching restaurants " + error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRestaurants();
+  }, []);
+
+	return (
+		<>
+			<div className="shows__section-container">
+				{
+					restaurants.map((restaurant) => {
+						return (
+							<li className="shows__container" key={restaurant.id}>
+							<div className="shows__container-buffer">
+								<div className="shows__headings-container">
+									<p className="shows__heading"></p>
+									<img className="shows__description--img" src={restaurant.image_url} alt="res Img" />
+								</div>
+								<div className="shows__headings-container">
+									<p className="shows__heading">Restaurant Name</p>
+									<p className="shows__description">{restaurant.name}</p>
+								</div>
+								<div className="shows__headings-container">
+									<p className="shows__heading">Location</p>
+									<p className="shows__description-location">{restaurant.address}</p>
+								</div>
+								<button className="shows__button">See Happy Hours Menu</button>
+							</div>
+						</li>
+						)
+					})
+				}
+			</div>
+		</>
+	)
 }
 
 export default Content; 
